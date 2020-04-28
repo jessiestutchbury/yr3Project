a5f300120c350628033493f60b264b45a9216389
jessiestutchbury <49071700+jessiestutchbury@users.noreply.github.com>
2020-04-28 17:16:01 +0100

    V7.1

---
+++ yr3Project-stock/storeFront/src/storeFront/Product.java
@@ -0,0 +1,9 @@
+package storeFront;
+
+public class Product {
+	public int id;
+	public String productName;
+	public String productDesc;
+	public Double price;
+	public int quantity;
+}
---
+++ yr3Project-stock/storeFront/src/storeFront/employees.java
@@ -0,0 +1,11 @@
+package storeFront;
+
+public class employees {
+	public int empID;
+	public String forename;
+	public String surname;
+	
+	public String emailAddress;
+	public int telephoneNo;
+	
+}
